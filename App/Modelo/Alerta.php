00cab86f3552b38ead14f520f6fd9251b57b35e9
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2022-11-19 17:31:10 -0400

    SE AGREGO UN NUEVO MODELO ALERTA

---
+++ App/Modelo/Alerta.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Modelo;
+
+trait Alerta
+{
+    public  $alerta_fallo = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                                <strong>Alerta!</strong> Algo sucedio en el registro vuelve a intentarlo.
+                                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                                <span aria-hidden='true'>&times;</span></button></div>",
+
+            $alerta_advertencia = "<div class='alert alert-warning alert-dismissible fade show' role='alert'><strong>
+                                    ¡Alerta!</strong> Completa todos los campos correctamente!
+                                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                                    <span aria-hidden='true'>&times;</span></button></div>",
+
+            $alerta_validacion = "<div class='alert alert-info alert-dismissible fade show' role='alert'>
+                                    <strong>¡Alerta!</strong> Completa cada campo correctamente!
+                                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                                    <span aria-hidden='true'>&times;</span></button></div>";
+}
+
+?>
