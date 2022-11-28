01c2abb3054e746c88136d62dc7bd4e5ada7cc0b
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2022-11-28 11:44:09 -0400

    SE AGREGO NUEVA ALERTA PARA ELIMINAR

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,20 +1,42 @@
-function advertencia(e){
+function advertencia(e) {
     e.preventDefault();
     var url = e.currentTarget.getAttribute('href');
+
     Swal.fire({
         title: '¿Estas seguro?',
-        text: "Recuerda el dato se eliminara permanentemente",
+        text: "Recuerda el dato se eliminara permanentemente!",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d22',
-        confirmButtonText: 'Si, eliminar!',
-        cancelButtonText: 'No, Cancelar!'
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, Eliminar!'
     }).then((result) => {
         if (result.isConfirmed) {
-            window.location.href=url;
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Eliminado correctamente',
+                showConfirmButton: false,
+                timer: 1000,
+            })
+            setTimeout(function() {
+                window.location.href = url;
+            }, 500);
         }
-    });
+    })
 };
 
-
+// function guardar(e) {
+//     e.preventDefault();
+//     var url = e.currentTarget.getAttribute('href');
+//     Swal.fire({
+//         position: 'top-end',
+//         icon: 'success',
+//         title: 'Guardado correctamente',
+//         showConfirmButton: false,
+//         timer: 1000
+//     })
+//     setTimeout(function() {
+//         window.location.href = url;
+//     }, 500);
+// };
